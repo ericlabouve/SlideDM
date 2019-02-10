bf175abdd1b4bf9d4fd04349f1d3855a58b5f51b
Eric LaBouve <elabouve@csc235x02.ad.calpoly.edu>
2019-02-10 01:24:42 -0800

    Beautiful Edit Profile screen

--- SetupProfileViewController.swift
+++ SetupProfileViewController.swift
@@ -7,16 +7,46 @@
 
 import UIKit
 
-class SetupProfileViewController: UIViewController {
+extension UITextField {
+    func underlined(){
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.white.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
+
+class SetupProfileViewController: UIViewController, UITextFieldDelegate {
     
+    var backgroundImageView = UIImageView()
     @IBOutlet weak var helloLabel: UILabel!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var whyPhoneNumberButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var userFirstName: String = ""
+    var userLastName: String = ""
+    var userEncryptedPhoneNumber: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        phoneNumberTextField.delegate = self
+        // TextField Background
+        firstNameTextField.backgroundColor = UIColor.clear
+        lastNameTextField.backgroundColor = UIColor.clear
+        phoneNumberTextField.backgroundColor = UIColor.clear
+        // TextField Bottom Border
+        firstNameTextField.underlined()
+        lastNameTextField.underlined()
+        phoneNumberTextField.underlined()
+        setBackground()
         hideUI()
     }
     
@@ -36,7 +66,7 @@ class SetupProfileViewController: UIViewController {
         super.viewDidAppear(animated)
         // Greet the user
         // Fade "Hello!" in
-        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
             self.helloLabel.alpha = 1
         }, completion: { _ in
             // Fade "Hello!" out
@@ -49,21 +79,70 @@ class SetupProfileViewController: UIViewController {
                     self.lastNameTextField.alpha = 1
                     self.phoneNumberTextField.alpha = 1
                     self.whyPhoneNumberButton.alpha = 1
+                    self.doneButton.alpha = 1
                 })
             })
         })
     }
     
+    func outAnimation() {
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
+            self.firstNameTextField.alpha = 0
+            self.lastNameTextField.alpha = 0
+            self.phoneNumberTextField.alpha = 0
+            self.whyPhoneNumberButton.alpha = 0
+            self.doneButton.alpha = 0
+        })
+    }
+    
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundImageView.image = UIImage(named: "tinyRocket")
+        view.sendSubviewToBack(backgroundImageView)
+    }
+    
     @IBAction func doneButton(_ sender: UIButton) {
-        let number = LoginViewController.cleanPhoneNumber(phoneNumber: phoneNumberTextField.text!)
-        if number != nil {
-            userPhoneNumber = number!.sha256()
+        var done = true
+        // Check first name
+        if let _userFirstName = firstNameTextField.text {
+            userFirstName = _userFirstName
+        } else {
+            done = false
+        }
+        // Check last name
+        if let _userLastName = lastNameTextField.text {
+            userLastName = _userLastName
+        } else {
+            done = false
+        }
+        // Check phone number
+        if let number = LoginViewController.cleanPhoneNumber(phoneNumber: phoneNumberTextField.text!) {
+            userEncryptedPhoneNumber = number.sha256()
+        } else {
+            done = false
+        }
+        if done {
+            // Move this to viewWillDisappear when setting up next view controller
+            outAnimation()
+            // Move to next screen
+        } else {
+            // Complain to the user
+            let message = "Please fill out all the fields. Make sure that your phone number has the correct amount of digits."
+            let alert = UIAlertController(title: "User Alert", message: message, preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(defaultAction)
+            present(alert, animated: true, completion:nil)
         }
     }
     
     @IBAction func whyPhoneNumberButton(_ sender: UIButton) {
-        let message = "SlideDM uses phone numbers to uniquely identify our users and we need your explicit permission to obtain them. We encrypt all phone numbers and then throw away the key so that no one can take advantage of your personal data."
-        let alert = UIAlertController(title: "Phone Numbers", message: message, preferredStyle: .alert)
+        let message = "SlideDM uses phone numbers to uniquely identify our users and we need your explicit permission to obtain your phone number. We encrypt all phone numbers so that no one can take advantage of your personal data."
+        let alert = UIAlertController(title: "Why My Phone Number?", message: message, preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "OK", style: .default)
         alert.addAction(defaultAction)
         present(alert, animated: true, completion:nil)
@@ -76,6 +155,22 @@ class SetupProfileViewController: UIViewController {
         lastNameTextField.alpha = 0
         phoneNumberTextField.alpha = 0
         whyPhoneNumberButton.alpha = 0
+        doneButton.alpha = 0
+    }
+    
+    /**
+     * Called when 'return' key pressed. return NO to ignore.
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    /**
+     * Called when the user click on the view (outside the UITextField).
+     */
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }
